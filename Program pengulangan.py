7ee6499f923b01b7ef99accee939c33db7b5ffca
Arief <ariefwicaksonoooo@gmail.com>
2023-11-14 20:13:45 +0700

    #LOOPING

---
+++ Program pengulangan.py
@@ -0,0 +1,10 @@
+#PENGULANGAN
+jumlah_buku = 99
+print('ibu berkata, "baca semua buku"')
+jumlah_buku_yang_sudah_dibaca = 0
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+for jumlah_buku_yang_sudah_dibaca in range(1, 100):
+    print((f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca"))
+
+print(f'jumlah_buku_yang_sudah_dibaca {jumlah_buku_yang_sudah_dibaca}')
