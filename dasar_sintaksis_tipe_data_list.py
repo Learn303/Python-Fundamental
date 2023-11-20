8e103e68a9a8fb83649cd10adf83ddc2adcda34f
Arief <ariefwicaksonoooo@gmail.com>
2023-11-20 20:23:25 +0700

    tipe_data_list

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -27,3 +27,29 @@ print('\ntambakan beberapa buku baru')
 daftar_buku.append ('holla')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+
+
+print('\nClear list')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku)
+
+
+print('\nKembaikan awal nilai daftar buku dan ganti judul beberapa buku')
+daftar_buku = ['seven habbit', 'how to influence people', 'thing first thing', 'aadc']
+daftar_buku[0]='bad habbit'
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+print('\nAmbil beberapa element dari list dengan pop')
+daftar_buku.pop(2)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku)
+
+print('\nMengambil element dengan pop element terakhir')
+daftar_buku = ['seven habbit', 'how to influence people', 'thing first thing', 'aadc']
+daftar_buku.pop(-1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
